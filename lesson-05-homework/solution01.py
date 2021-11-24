8e037201be1e27b1306919444263b5307ba8fa23
m0shanya <funnytimka@gmail.com>
2021-11-24 19:43:13 +0300

    Improving the solution01.py

--- lesson-05-homework/solution01.py
+++ lesson-05-homework/solution01.py
@@ -1,22 +1,23 @@
-"""
-Создайте функцию three_args(), которая принимает 1, 2 или 3 ключевых параметра.  
-В результате ее работы на печать выводятся значения переданных переменных, но только если они не равны None. 
-Получим, например, следующее сообщение: Переданы аргументы: var1 = 2, var3 = 10.
-"""
-def three_args(spis):
+def three_args(*spis):
     j = 1
     for i in spis:
+        if i == None:
+            j += 1
         if i != None:
             print(f"elements passed:\nvar{j} = {i}")
             j += 1
     return
 
 
-a = int(input("Input any number:\n"))
-b = int(input("Input any number:\n"))
-c = int(input("Input any number:\n"))
-spis = []
-spis.append(a)
-spis.append(b)
-spis.append(c)
-three_args(spis)
+k = int(input("Test(1) or input from keyboard(2)?"))
+if k == 2:
+    a = int(input("Input any number:\n"))
+    b = int(input("Input any number:\n"))
+    c = int(input("Input any number:\n"))
+    spis = []
+    spis.append(a)
+    spis.append(b)
+    spis.append(c)
+    three_args(*spis)
+else:
+    three_args(None, 1, 2)
