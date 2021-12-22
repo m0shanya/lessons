3f26929bda209b7f0bb1f79c4240083f7b0e49a3
m0shanya <funnytimka@gmail.com>
2021-12-22 19:45:55 +0300

    maybe work, idk...

---
+++ lesson-11/main.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+
+def create_product_table():
+    with sqlite3.connect("products.sqlite3") as session:
+        cursor = session.cursor()
+        cursor.execute(
+            """
+            CREATE TABLE products (
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               name VARCHAR,
+               price INTEGER,
+               count INTEGER,
+               comment VARCHAR,
+               datetime DATETIME
+            );
+            """,
+        )
+        session.commit()
+
+
+def create_product(name: str, price: int, count: int, comment: str):
+    with sqlite3.connect("products.sqlite3") as session:
+        cursor = session.cursor()
+        cursor.execute(
+            f"""
+               INSERT INTO products (name, price, count, comment)
+               VALUES (?, ?, ?, ?);
+               """,
+            (name, price, count, comment),
+        )
+        session.commit()
+
+
+create_product("test", 14, 134, "rr")
